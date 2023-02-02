c7c9355a9bff49af195b98121a78598bcb822797
eydellnet06 <eydellnet06@EYDNET35>
2023-02-02 15:15:01 +0530

    Add project files.

---
+++ ClassExample_Feb02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample_Feb02
+{
+    class Automobile
+    {
+        string model = "BMW";
+        string color = "Red";
+        string type = "FourWheeler";
+        static void Main(string[] args)
+        {
+            Automobile objectOne = new Automobile();
+            Console.WriteLine("Model : " + objectOne.model + "\nColor : " + objectOne.color + "\nType : " + objectOne.type);
+        }
+    }
+}
